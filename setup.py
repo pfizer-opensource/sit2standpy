2af907ae9d43dad1e92bab6a1caaf16422dbec51
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-07-17 15:45:06 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pysit2stand",
+    version="2019.07",
+    author="Lukas Adamowicz",
+    author_email="lukas.adamowicz@pfizer.com",
+    description="Sit-to-stand detection using a single lumbar-mounted accelerometer.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PfizerRD/pysit2stand",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
