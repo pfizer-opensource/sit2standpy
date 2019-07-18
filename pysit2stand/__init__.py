4ba77a6bbc2f7a4ec2da7fd7dad11049f2ba16a5
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-07-18 09:45:45 -0400

    Renamed wavelet.py to core.py to make more sense

--- pysit2stand/__init__.py
+++ pysit2stand/__init__.py
@@ -1,4 +1,4 @@
-from pysit2stand.wavelet import Sit2Stand
+from pysit2stand.core import Sit2Stand
 from pysit2stand import detectors
 from pysit2stand.utility import AccFilter, mov_stats, Transition
 from pysit2stand.quantify import TransitionQuantifier
