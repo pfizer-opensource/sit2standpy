fb246bf21d2382f37da6f9f3ebcfad731b82c930
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-04 12:56:46 -0400

    Added test for Transition class

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,8 +1,10 @@
 from numpy import array
 import pysit2stand as s2s
+from pandas import to_datetime
 
 
-def test_utility():
+# UTILITY
+def test_mov_stats():
     x = array([0, 1, 2, 3, 4, 5])
 
     mn, sd, pad = s2s.utility.mov_stats(x, 3)
@@ -10,3 +12,12 @@ def test_utility():
     assert (mn == array([1., 1., 1., 2., 3., 4.])).all()
     assert (sd == array([1., 1., 1., 1., 1., 1.])).all()
     assert pad == 2
+
+
+def test_transition():
+    t1 = to_datetime(1567616049649, unit='ms')
+    t2 = to_datetime(1567616049649 + 1e3, unit='ms')
+
+    trans = s2s.Transition(times=(t1, t2))
+
+    assert trans.duration == 1.
