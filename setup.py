f88ea34e61562ada3a8c91e5bf0a3c65a0d38653
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-04 13:39:50 -0400

    Added ability to include other files in package (data file) Added MANIFEST file

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PfizerRD/pysit2stand",
+    include_pacakge_data=True,
     packages=setuptools.find_packages(),
     license='MIT',
     classifiers=[
