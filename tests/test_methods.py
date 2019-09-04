ca621a930e1c355fbabd75b50c2332f5356da87c
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-04 13:30:04 -0400

    Split algorithm testing into stillness and displacement methods Added displacement method testing Created a sample data file

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -23,7 +23,7 @@ def test_transition():
     assert trans.duration == 1.
 
 
-def test_module():
+def test_stillness():
     data = loadtxt('sample.csv', delimiter=',')
     time = data[:, 0]
     acc = data[:, 1:]
@@ -45,4 +45,29 @@ def test_module():
     SiSt = wavelet_ptd.fit(acc, a_time, still, a_filt, fs=128)
 
     assert len(SiSt) == 4
-    assert allclose(array([SiSt[i].duration for i in SiSt]), array([1.984, 1.602, 1.461, 1.461]), atol=5e-4)
+    assert allclose(array([SiSt[i].duration for i in SiSt]), array([1.984, 1.602, 1.461, 1.461]), atol=1e-3)
+
+
+def test_displacement():
+    data = loadtxt('sample.csv', delimiter=',')
+    time = data[:, 0]
+    acc = data[:, 1:]
+    a_time = to_datetime(time, unit='ms', utc=True).tz_convert('EST').tz_localize(None)
+
+    a_filt = s2s.AccFilter(lowpass_order=4, lowpass_cutoff=5, discrete_wavelet='dmey',
+                           reconstruction_level=1, reconstruction_method='moving average')
+
+    ths = {'stand displacement': 0.125, 'transition velocity': 0.2, 'accel moving avg': 0.2,
+           'accel moving std': 0.1, 'jerk moving avg': 2.5, 'jerk moving std': 3}
+    disp = s2s.detectors.Displacement(gravity=9.81, thresholds=ths, long_still=0.5, moving_window=0.3,
+                                      duration_factor=10, displacement_factor=0.75,
+                                      lmax_kwargs=None, lmin_kwargs={'height': -9.5})
+    cwave = 'gaus1'
+    pk_pwr = [0, 0.5]
+    pk_pwr_kw = {'height': 90, 'distance': 128}
+    wavelet_ptd = s2s.Sit2Stand(continuous_wavelet=cwave, peak_pwr_band=pk_pwr, peak_pwr_par=pk_pwr_kw)
+
+    SiSt = wavelet_ptd.fit(acc, a_time, disp, a_filt, fs=128)
+
+    assert len(SiSt) == 4
+    assert allclose(array([SiSt[i].duration for i in SiSt]), array([1.984, 1.602, 1.461, 1.461]), atol=1e-3)
