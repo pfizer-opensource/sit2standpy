9f8fa9510863ba018b99933408dd44a56a875b08
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-05 08:33:37 -0400

    Added the SPARC smoothness method

--- pysit2stand/quantify.py
+++ pysit2stand/quantify.py
@@ -5,6 +5,8 @@ Lukas Adamowicz
 July 2019
 Pfizer
 """
+from numpy import ceil, log2, arange, abs, sum, sqrt, diff
+from scipy.fftpack import fft
 from pysit2stand.utility import Transition
 
 
@@ -58,3 +60,65 @@ class TransitionQuantifier:
                                       min_v_velocity=min_v_vel, max_acceleration=max_acc, min_acceleration=min_acc)
 
         return self.transition_
+
+    @staticmethod
+    def sparc(x, fs, padlevel=4, fc=10.0, amp_th=0.05):
+        """
+        SPectral ARC length metric for quantifying smoothness
+
+        Parameters
+        ----------
+        x : numpy.ndarray
+            Array containing the data to be analyzed for smoothness
+        fs : float
+            Sampling frequency
+        padlevel : int, optional
+            Indicates the amount of zero-padding to be done to the movement data for estimating the spectral arc length.
+            Default is 4.
+        fc : float, optional
+            The max cutoff frequency for calculating the spectral arc length metric. Default is 10.0
+        amp_th : float, optional
+            The amplitude threshold to be used for determining the cut off frequency up to which the spectral arc length
+            is to be estimated. Default is 0.05
+
+        Returns
+        -------
+        sal : float
+            The spectral arc length estimate of the given data's smoothness
+        (f, Mf) : (numpy.ndarray, numpy.ndarray)
+            The frequency and the magnitude spectrum of the input data. This spectral is from 0 to the Nyquist frequency
+        (f_sel, Mf_sel) : (numpy.ndarray, numpy.ndarray)
+            The portion of the spectrum that is selected for calculating the spectral arc length
+
+        References
+        ----------
+        S. Balasubramanian, A. Melendez-Calderon, A. Roby-Brami, E. Burdet. ``On the analysis of movement smoothness.''
+        Journal of NeuroEngineering and Rehabilitation. 2015.
+        """
+        # number of zeros to be padded
+        nfft = int(pow(2, ceil(log2(len(x))) + padlevel))
+
+        # frequency
+        f = arange(0, fs, fs / nfft)
+        # normalized magnitude spectrum
+        Mf = abs(fft(x, nfft))
+        Mf = Mf / Mf.max()
+
+        # indices to choose only the spectrum withing the given cutoff frequency Fc
+        # NOTE: this is a low pass filtering operation to get rid of high frequency noise from affecting the next step
+        # (amplitude threshold based cutoff for arc length calculation
+        fc_inx = ((f <= fc) * 1).nonzero()
+        f_sel = f[fc_inx]
+        Mf_sel = Mf[fc_inx]
+
+        # choose the amplitude threshold based cutoff frequency. Index of the last point on the magnitude spectrum is
+        # greater than or equal to the amplitude threshold
+        inx = ((Mf_sel >= amp_th) * 1).nonzero()[0]
+        fc_inx = range(inx[0], inx[-1] + 1)
+        f_sel = f_sel[fc_inx]
+        Mf_sel = Mf_sel[fc_inx]
+
+        # calculate the arc length
+        sal = -sum(sqrt(pow(diff(f_sel) / (f_sel[-1] - f_sel[0]), 2) + pow(diff(Mf_sel), 2)))
+
+        return sal, (f, Mf), (f_sel, Mf_sel)
