b0b49cdcfc6e5cf376bc9b471eeea49141c432aa
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-17 16:02:39 -0400

    Removed numpydoc

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ release = '1.0.0rc4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.intersphinx']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx']  # numpydoc
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
