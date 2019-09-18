5e16523e06ddcea465353129b3818f49978bc8f8
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-09-18 10:30:05 -0400

    Adding links to documentation, and PyPI

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PfizerRD/pysit2stand",
+    download_url="https://pypi.org/project/pysit2stand/",
+    project_urls={
+        "Documentation": "https://pysit2stand.readthedocs.io/en/latest/"
+    },
     include_pacakge_data=True,
     package_data={'pysit2stand': ['data/*.csv']},
     packages=setuptools.find_packages(),
