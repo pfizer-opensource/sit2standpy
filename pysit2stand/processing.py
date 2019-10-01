1bcb800ff70605359e5b01e08978ce81517c1917
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-01 11:03:04 -0400

    Added a new AccelerationFilter that also performs the power summation and peak finding

--- pysit2stand/processing.py
+++ pysit2stand/processing.py
@@ -5,13 +5,153 @@ Lukas Adamowicz
 Pfizer
 2019
 """
-from numpy import around, mean, diff, timedelta64
+from numpy import around, mean, diff, timedelta64, arange, logical_and, sum, std
 from numpy.linalg import norm
-from scipy.signal import butter, filtfilt
+from scipy.signal import butter, filtfilt, find_peaks
 import pywt
 from pandas import to_datetime
 
 from pysit2stand.utility import mov_stats
+
+
+class AccelerationFilter:
+    def __init__(self, continuous_wavelet='gaus1', power_band=[0, 0.5], power_peak_kw=None, power_std_height=True,
+                 reconstruction_method='moving average', lowpass_order=4, lowpass_cutoff=5, window=0.25,
+                 discrete_wavelet='dmey', extension_mode='constant', reconstruction_level=1):
+        """
+        Object for filtering and reconstructing raw acceleration data
+
+        Parameters
+        ----------
+        continuous_wavelet : str, optional
+            Continuous wavelet to use for signal deconstruction. Default is 'gaus1'. CWT coefficients will be summed
+            in frequency bands that will be used for detecting approximate STS locations.
+        power_band : {array_like, int, float}, optional
+            Frequency band in which to sum the CWT coefficients. Either an array_like of length 2, with the lower and
+            upper limits, or a number, which will be taken as the upper limit, and the lower limit will be set to 0.
+            Default is [0, 0.5].
+        power_peak_kw : {None, dict}, optional
+            Extra key-word arguments to pass to scipy.signal.find_peaks when finding peaks in the
+            summed CWT coefficient power band data. Default is None, which will use the default parameters except
+            setting minimum height to 90, unless std_height is True.
+        power_std_height : bool, optional
+            Use the standard deviation of the power for peak finding. Default is True. If True, the standard deviation
+            height will overwrite the setting in `power_peak_kw`.
+        reconstruction_method : {'moving average', 'dwt'}, optional
+            Method for computing the reconstructed acceleration. Default is 'moving average', which takes the moving
+            average over the specified window. Other option is 'dwt', which uses the discrete wavelet transform to
+            deconstruct and reconstruct the signal while filtering noise out.
+        lowpass_order : int, optional
+            Initial low-pass filtering order. Default is 4.
+        lowpass_cutoff : float, optional
+            Initial low-pass filtering cuttoff, in Hz. Default is 5Hz.
+        window : float, optional
+            Window to use for moving average, in seconds. Default is 0.25s. Ignored if reconstruction_method is 'dwt'.
+        discrete_wavelet : str, optional
+            Discrete wavelet to use if reconstruction_method is 'dwt'. Default is 'dmey'. See
+            pywt.wavelist(kind='discrete') for a complete list of options. Ignored if reconstruction_method is
+            'moving average'.
+        extension_mode : str, optional
+            Signal extension mode to use in the DWT de- and re-construction of the signal. Default is 'constant', see
+            pywt.Modes.modes for a list of options. Ignored if reconstruction_method is 'moving average'.
+        reconstruction_level : int, optional
+            Reconstruction level of the DWT processed signal. Default is 1. Ignored if reconstruction_method is
+            'moving average'.
+        """
+        self.cwave = continuous_wavelet
+        if isinstance(power_band, (int, float)):
+            self.power_start_f = 0
+            self.power_end_f = power_band
+        else:
+            self.power_start_f = power_band[0]
+            self.power_end_f = power_band[1]
+
+        self.std_height = power_std_height
+        if power_peak_kw is None:  # if not set, set the default values
+            # default height is 90. this will be reset later if necessary if using the stdev height
+            self.power_peak_kw = {'height': 90}
+        else:
+            self.power_peak_kw = power_peak_kw  # if passed as a dictionary
+
+        self.method = reconstruction_method
+
+        self.lp_order = lowpass_order
+        self.lp_cut = lowpass_cutoff
+
+        self.window = window
+
+        self.dwave = discrete_wavelet
+        self.ext_mode = extension_mode
+        self.recon_level = reconstruction_level
+
+    def apply(self, accel, fs):
+        """
+        Apply the desired filtering to the provided signal.
+
+        Parameters
+        ----------
+        accel : numpy.ndarray
+            (N, 3) array of raw acceleration values.
+        fs : float, optional
+            Sampling frequency for the acceleration data.
+
+        Returns
+        -------
+        mag_acc_f : numpy.ndarray
+            (N, ) array of the filtered (low-pass only) acceleration magnitude.
+        mag_acc_r : numpy.ndarray
+            (N, ) array of the reconstructed acceleration magnitude. This is either filtered and then moving averaged,
+            or filtered, and then passed through the DWT and inverse DWT with more filtering, depending on the
+            reconstruction_method specified.
+        power : numpy.ndarray
+            (N, ) array of the CWT power approximation in the band specified by `power_band`.
+        power_peaks : numpy.ndarray
+            Indices of the peaks detected in the power signal.
+        """
+        # compute the acceleration magnitude
+        macc = norm(accel, axis=1)
+
+        # setup the filter, and filter the acceleration magnitude
+        fc = butter(self.lp_ord, 2 * self.lp_cut / fs, btype='low')
+        macc_f = filtfilt(fc[0], fc[1], macc)
+
+        if self.method == 'dwt':
+            # deconstruct the filtered acceleration magnitude
+            coefs = pywt.wavedec(macc_f, self.dwave, mode=self.ext_mode)
+
+            # set all but the desired level of coefficients to be 0s
+            if (len(coefs) - self.recon_level) < 1:
+                print(f'Chosen reconstruction level is too high, setting reconstruction level to {len(coefs) - 1}')
+                ind = 1
+            else:
+                ind = len(coefs) - self.recon_level
+
+            for i in range(1, len(coefs)):
+                if i != ind:
+                    coefs[i][:] = 0
+
+            macc_r = pywt.waverec(coefs, self.dwave, mode=self.ext_mode)
+        elif self.method == 'moving average':
+            n_window = int(around(fs * self.window))  # compute the length in samples of the moving average
+            macc_r, _, _ = mov_stats(macc_f, n_window)  # compute the moving average
+
+        # ---------------------------------------------------
+        # CWT power peak detection
+
+        # compute the cwt
+        coefs, freqs = pywt.cwt(macc_r, arange(1, 65), self.cwave, sampling_period=1/fs)
+
+        # sum the coefficients over the frequencies in the power band
+        f_mask = logical_and(freqs <= self.power_end_f, freqs >= self.power_start_f)
+        power = sum(coefs[f_mask, :], axis=0)
+
+        # find the peaks in the power data
+        if self.std_height:
+            self.power_peak_kw['height'] = std(power, ddof=1)
+
+        power_peaks, _ = find_peaks(power, **self.power_peak_kw)
+
+        return macc_f, macc_r[:macc_f.size], power, power_peaks
 
 
 class AccFilter:
