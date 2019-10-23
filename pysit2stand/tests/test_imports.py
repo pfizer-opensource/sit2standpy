0d0824ae7cdac87513fc91d24ca21b3d882841ec
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-23 14:45:13 -0400

    Update pysit2stand import testing

--- pysit2stand/tests/test_imports.py
+++ pysit2stand/tests/test_imports.py
@@ -21,9 +21,8 @@ def test_pywt():
 
 def test_pysit2stand():
     import pysit2stand
-    from pysit2stand.utility import Transition, AccFilter, mov_stats
-    from pysit2stand import Sit2Stand
-    from pysit2stand.quantify import TransitionQuantifier
+    from pysit2stand import Sit2Stand, AutoSit2Stand, detectors, mov_stats, Transition, TransitionQuantifier, \
+        AccelerationFilter, AccFilter, process_timestamps, __version__
     from pysit2stand.detectors import Stillness, Displacement
 
     return
