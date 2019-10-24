12791d7899f94222a261238c1eb712968d479f69
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-24 09:22:29 -0400

    Add test for AccelerationFilter power_band with float input

--- pysit2stand/tests/conftest.py
+++ pysit2stand/tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
-from pandas import to_datetime
-from numpy import loadtxt
+from pandas import to_datetime, date_range
+from numpy import loadtxt, random
 from importlib import resources
 
 
@@ -21,6 +21,15 @@ def raw_accel():
         acc = loadtxt(file_path, dtype=float, delimiter=',', usecols=(1, 2, 3))
 
     return acc
+
+
+@pytest.fixture
+def time():
+    # pull sample time data
+    with resources.path('pysit2stand.data', 'sample.csv') as file_path:
+        time = loadtxt(file_path, dtype=float, delimiter=',', usecols=0)
+
+    return time
 
 
 @pytest.fixture
@@ -59,3 +68,30 @@ def power_peaks():
     power_peaks = power_peaks[power_peaks != -1]  # remove filler values
 
     return power_peaks
+
+
+@pytest.fixture
+def overnight_time_accel():
+    # generate some time that spans overnight, to testing windowing
+    ts = date_range(start='2019-10-10 16:00', end='2019-10-11 12:00', freq='1H').astype(int)
+    # generate random acceleration values with the same shape
+    acc = random.rand(ts.size)
+
+    return ts, acc
+
+
+@pytest.fixture
+def windowed_timestamps():
+    ts = to_datetime(['2019-10-10 16:00:00', '2019-10-10 17:00:00', '2019-10-10 18:00:00', '2019-10-10 19:00:00',
+                      '2019-10-10 20:00:00', '2019-10-11 08:00:00', '2019-10-11 09:00:00', '2019-10-11 10:00:00',
+                      '2019-10-11 11:00:00', '2019-10-11 12:00:00'], format='%Y-%m-%d %H:%M:%S')
+    return ts
+
+
+@pytest.fixture
+def timestamps_time_accel():
+    ts = date_range(start='2019-10-10 16:00', end='2019-10-10 16:02', freq='0.05S')
+    time = ts.astype(int)
+    acc = random.rand(ts.size)
+
+    return ts, time, acc
--- pysit2stand/tests/test_processing.py
+++ pysit2stand/tests/test_processing.py
@@ -11,6 +11,13 @@ def test_accelerationfilter_none_inputs():
     assert af.power_peak_kw == {'height': 90}
 
 
+def test_accelerationfilter_single_power_band_input():
+    af = AccelerationFilter(power_band=0.8)
+
+    assert isclose(af.power_start_f, 0.0)
+    assert isclose(af.power_end_f, 0.8)
+
+
 def test_accelerationfilter(raw_accel, filt_accel, rm_accel, power, power_peaks):
     af = AccelerationFilter(continuous_wavelet='gaus1', power_band=[0, 0.5], power_peak_kw={'distance': 128},
                             power_std_height=True, reconstruction_method='moving average', lowpass_order=4,
@@ -22,3 +29,27 @@ def test_accelerationfilter(raw_accel, filt_accel, rm_accel, power, power_peaks)
     assert allclose(rm_accel, rm_acc)
     assert allclose(power, pwr)
     assert allclose(power_peaks, pwr_pk)
+
+
+def test_process_timestamps_error(time, raw_accel):
+    with pytest.raises(ValueError) as e_info:
+        process_timestamps(time, raw_accel)
+
+
+def test_process_timestamps_window(overnight_time_accel, windowed_timestamps):
+    timestamps, dt, accel = process_timestamps(overnight_time_accel[0], overnight_time_accel[1], time_units='ns',
+                                               conv_kw={}, window=True, hours=('08:00', '20:00'))
+
+    assert timestamps.size == 10
+    assert all(timestamps == windowed_timestamps)
+    assert accel.size == 10
+    assert isclose(dt, 3600.0)
+
+
+def test_process_timestamps(timestamps_time_accel):
+    timestamps, dt = process_timestamps(timestamps_time_accel[1], timestamps_time_accel[2], time_units='ns',
+                                        window=False)
+
+    assert all(timestamps == timestamps_time_accel[0])
+    assert isclose(dt, 0.05)
+
