72042ef1754cacbdb0bcc1e7dbc3899745599720
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-24 09:28:04 -0400

    Update name of RM filtered data to be more descriptive

--- pysit2stand/tests/test_processing.py
+++ pysit2stand/tests/test_processing.py
@@ -17,17 +17,19 @@ class TestAccelerationFilter:
         assert isclose(af.power_start_f, 0.0)
         assert isclose(af.power_end_f, 0.8)
 
-    def test(self, raw_accel, filt_accel, rm_accel, power, power_peaks):
+    def test_rm(self, raw_accel, filt_accel_rm, rm_accel_rm, power_rm, power_peaks_rm):
         af = AccelerationFilter(continuous_wavelet='gaus1', power_band=[0, 0.5], power_peak_kw={'distance': 128},
                                 power_std_height=True, reconstruction_method='moving average', lowpass_order=4,
                                 lowpass_cutoff=5, window=0.25)
 
         f_acc, rm_acc, pwr, pwr_pk = af.apply(raw_accel, 128)
 
-        assert allclose(filt_accel, f_acc)
-        assert allclose(rm_accel, rm_acc)
-        assert allclose(power, pwr)
-        assert allclose(power_peaks, pwr_pk)
+        assert allclose(filt_accel_rm, f_acc)
+        assert allclose(rm_accel_rm, rm_acc)
+        assert allclose(power_rm, pwr)
+        assert allclose(power_peaks_rm, pwr_pk)
+
+    def test_dwt(self, raw_accel, filt_accel, rec_accel, power, power_peaks):
 
 
 class TestProcessTimestamps:
