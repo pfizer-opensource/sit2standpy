26ba9d9919f2a1fdd5be7b1aca4d94a10fc3302a
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-24 08:47:23 -0400

    Add test for AccelerationFilter

--- pysit2stand/tests/test_processing.py
+++ pysit2stand/tests/test_processing.py
@@ -9,3 +9,16 @@ def test_accelerationfilter_none_inputs():
     assert isclose(af.power_start_f, 0.0)
     assert isclose(af.power_end_f, 0.5)
     assert af.power_peak_kw == {'height': 90}
+
+
+def test_accelerationfilter(raw_accel, filt_accel, rm_accel, power, power_peaks):
+    af = AccelerationFilter(continuous_wavelet='gaus1', power_band=[0, 0.5], power_peak_kw={'distance': 128},
+                            power_std_height=True, reconstruction_method='moving average', lowpass_order=4,
+                            lowpass_cutoff=5, window=0.25)
+
+    f_acc, rm_acc, pwr, pwr_pk = af.apply(raw_accel, 128)
+
+    assert allclose(filt_accel, f_acc)
+    assert allclose(rm_accel, rm_acc)
+    assert allclose(power, pwr)
+    assert allclose(power_peaks, pwr_pk)
