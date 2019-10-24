66b880660509e19b4816cdf3c42e50f39fe449d8
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-24 10:04:47 -0400

    Remove old functions

--- pysit2stand/__init__.py
+++ pysit2stand/__init__.py
@@ -1,6 +1,6 @@
-from pysit2stand.core import Sit2Stand, AutoSit2Stand
+from pysit2stand.core import Sit2Stand
 from pysit2stand import detectors
 from pysit2stand.utility import mov_stats, Transition
-from pysit2stand.processing import AccelerationFilter, AccFilter, process_timestamps
+from pysit2stand.processing import AccelerationFilter, process_timestamps
 from pysit2stand.quantify import TransitionQuantifier
 from pysit2stand.version import __version__
