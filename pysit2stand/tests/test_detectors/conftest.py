1ee3ad5a36752cb692522883a168a1ced2a1aa05
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2019-10-24 12:37:34 -0400

    Add get_still test

--- pysit2stand/tests/test_detectors/conftest.py
+++ pysit2stand/tests/test_detectors/conftest.py
@@ -1,14 +1,45 @@
 from pytest import fixture
-from numpy import sin, cos, arange, pi
+from numpy import sin, cos, arange, pi, zeros
+import h5py
 
 
 @fixture
 def integrate_data():
     dt = 0.01
     x = arange(0, 2 * pi, dt)
-
     f = cos(x)
+
     F = sin(x)
     FF = -cos(x) + 1  # need to add one due to intial value of the integration being 0
 
     return f, F, FF, dt
+
+
+@fixture
+def stillness():
+    file_name = '/Users/LukasAdamowicz/Documents/Python Packages/Sit to Stand/pysit2stand/pysit2stand/data/' \
+                '.detector_results.h5'
+    with h5py.File(file_name, 'r') as file:
+        still = file['stillness'][()]
+
+    return still
+
+
+@fixture
+def still_starts():
+    file_name = '/Users/LukasAdamowicz/Documents/Python Packages/Sit to Stand/pysit2stand/pysit2stand/data/' \
+                '.detector_results.h5'
+    with h5py.File(file_name, 'r') as file:
+        starts = file['starts'][()]
+
+    return starts
+
+
+@fixture
+def still_stops():
+    file_name = '/Users/LukasAdamowicz/Documents/Python Packages/Sit to Stand/pysit2stand/pysit2stand/data/' \
+                '.detector_results.h5'
+    with h5py.File(file_name, 'r') as file:
+        stops = file['starts'][()]
+
+    return stops
--- pysit2stand/tests/test_detectors/test_detectors.py
+++ pysit2stand/tests/test_detectors/test_detectors.py
@@ -4,6 +4,17 @@ from numpy import allclose
 from pysit2stand.detectors.detectors import _get_still, _integrate_acc, Stillness, Displacement
 
 
+def test_get_still(filt_accel_rm, stillness, still_starts, still_stops):
+    still, starts, stops = _get_still(filt_accel_rm, 1/128, window=0.3, gravity=9.81,
+                                      thresholds={'stand displacement': 0.125, 'transition velocity': 0.2,
+                                                  'accel moving avg': 0.2, 'accel moving std': 0.1,
+                                                  'jerk moving avg': 2.5, 'jerk moving std': 3})
+
+    assert allclose(still, stillness)
+    assert allclose(starts, still_starts)
+    assert allclose(stops, still_stops)
+
+
 def test_integrate_acc(integrate_data):
     vel, pos = _integrate_acc(integrate_data[0], integrate_data[-1], True)
 
