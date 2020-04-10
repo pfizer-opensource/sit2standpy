e74a9d150c10bd3f68b95e7b7f079d40765ff5da
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-04-10 08:43:01 -0400

    Add tests for acceleration filter

--- sit2standpy/v2/tests/test_processes.py
+++ sit2standpy/v2/tests/test_processes.py
@@ -2,6 +2,7 @@ import pytest
 from tempfile import TemporaryFile
 
 from sit2standpy.v2.day_window import WindowDays
+from sit2standpy.v2.Filters import AccelerationFilter
 
 from sit2standpy.v2.tests.conftest import BaseProcessTester
 
@@ -12,3 +13,19 @@ class TestWindowDays(BaseProcessTester):
         super().setup_class()
         cls.process = WindowDays(hours=[8, 20])
         cls.test_keys = ['Processed/Sit2Stand/Day 1/Indices']
+
+
+class TestAccelerationFilterMovingAverage(BaseProcessTester):
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+        cls.process = AccelerationFilter('gaus1', power_band=[0, 0.5], power_peak_kw=None, power_std_height=True,
+                                         power_std_trim=0.5, reconstruction_method='moving average',
+                                         lowpass_order=4, lowpass_cutoff=5, window=0.25)
+        cls.test_keys = [
+            'Processed/Sit2Stand/Day 1/Filtered Acceleration',
+            'Processed/Sit2Stand/Day 1/Reconstructed Acceleration',
+            'Processed/Sit2Stand/Day 1/Power',
+            'Processed/Sit2Stand/Day 1/Power Peaks'
+        ]
+        # TODO add tests for processing with indices, errors?
