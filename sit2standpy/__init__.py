c2b433cc0f86b29f1669aa336b83cae360b0e9a7
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-04-15 10:29:24 -0400

    Add v2 interface

--- sit2standpy/__init__.py
+++ sit2standpy/__init__.py
@@ -3,4 +3,5 @@ from sit2standpy import detectors
 from sit2standpy.utility import mov_stats, Transition
 from sit2standpy.processing import AccelerationFilter, process_timestamps
 from sit2standpy.quantify import TransitionQuantifier
+from sit2standpy import v2
 from sit2standpy.version import __version__
