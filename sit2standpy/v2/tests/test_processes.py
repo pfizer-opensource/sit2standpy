e336333d4a483580ce9a20c28b278da9efb1c5a1
Lukas Adamowicz <lukas.adamowicz@pfizer.com>
2020-04-15 08:19:51 -0400

    Add displacement Sequential tests

--- sit2standpy/v2/tests/test_processes.py
+++ sit2standpy/v2/tests/test_processes.py
@@ -103,3 +103,32 @@ class TestStillnessSequentialPipeline(BaseProcessTester):
             'Processed/Sit2Stand/Day 1/Stillness Method/Min. Accel.',
             'Processed/Sit2Stand/Day 1/Stillness Method/SPARC'
         ]
+
+
+class TestDisplacementSequentialPipeline(BaseProcessTester):
+    @classmethod
+    def setup_class(cls):
+        super().setup_class()
+
+        seq = Sequential()
+        seq.add(WindowDays(hours=[8, 20]))
+        seq.add(AccelerationFilter('gaus1', power_band=[0, 0.5], power_peak_kw={'distance': 128}, power_std_height=True,
+                                   power_std_trim=0, reconstruction_method='moving average', lowpass_order=4,
+                                   lowpass_cutoff=5, window=0.25))
+        seq.add(Detector(stillness_constraint=False, gravity=9.81, thresholds=None, gravity_pass_order=4,
+                         gravity_pass_cutoff=0.8, long_still=0.5, moving_window=0.3, duration_factor=10,
+                         displacement_factor=0.75))
+
+        cls.process = seq
+
+        cls.test_keys = [
+            'Processed/Sit2Stand/Day 1/Filtered Acceleration',
+            'Processed/Sit2Stand/Day 1/Reconstructed Acceleration',
+            'Processed/Sit2Stand/Day 1/Power',
+            'Processed/Sit2Stand/Day 1/Power Peaks',
+            'Processed/Sit2Stand/Day 1/Displacement Method/STS Times',
+            'Processed/Sit2Stand/Day 1/Displacement Method/Duration',
+            'Processed/Sit2Stand/Day 1/Displacement Method/Max. Accel.',
+            'Processed/Sit2Stand/Day 1/Displacement Method/Min. Accel.',
+            'Processed/Sit2Stand/Day 1/Displacement Method/SPARC'
+        ]
